fc1967afe79bbf9a1d208d15abb92537a87b0697
Anele Noroita <36598042+anoroita@users.noreply.github.com>
2020-02-20 09:26:48 +0200

    Php Mailer not fixed

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -25,20 +25,20 @@ $headers = "From: noreply@gmail.com\n"; // This is the email address the generat
 $headers .= "Reply-To: $email_address";
 
 //PHPMailer
-$mail = new PHPMailer(); // create a new object
-$mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
-$mail->IsHTML(true);
-$mail->Username = $to;
-$mail->Password = "Fortunate92?";
-$mail->SetFrom($email_address);
-$mail->Subject = $email_subject;
-$mail->Body = $email_body;
-$mail->AddAddress($to);
+// $mail = new PHPMailer(); // create a new object
+// $mail->IsSMTP(); // enable SMTP
+// $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+// $mail->SMTPAuth = true; // authentication enabled
+// $mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+// $mail->Host = "smtp.gmail.com";
+// $mail->Port = 465; // or 587
+// $mail->IsHTML(true);
+// $mail->Username = $to;
+// $mail->Password = "Fortunate92?";
+// $mail->SetFrom($email_address);
+// $mail->Subject = $email_subject;
+// $mail->Body = $email_body;
+// $mail->AddAddress($to);
 
  if(!$mail->Send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
